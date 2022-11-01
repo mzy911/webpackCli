d53b56cedeeed4b3780eb372c800f12e75781492
mengfanrong <mfrwyy@163.com>
2022-11-01 10:37:37 +0800

    feat：模块懒加载、预加载

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+
+
+// 模块懒加载、预加载
+const Home = lazy(() => import(/* webpackChunkName:'home', webpackPrefetch */ '@/views/Home/index'))
+const About = lazy(() => import(/* webpackChunkName:'About', webpackPrefetch */ '@/views/About/index'))
