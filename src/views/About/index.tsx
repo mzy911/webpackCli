a89eb0cef2d7a96a37ee7618ad20b4f7ae87804e
mzy <mfrwyy@163.com>
2023-01-08 16:13:55 +0800

    ts 文件声明

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './index.less'
+import $ from 'jquery'
 
 import '@/public/styles/global.less'
 
 const About = () => {
+
+  
+  console.log('keyPartial', $)
+
+  
   return (
     <div className='mt_20 b_red'>
       <Link to='/page-detail/1'> 去详情页</Link>
