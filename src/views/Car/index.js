4fef1d817b1ead6e6018bbf8526b59da1aea348f
孟凡荣 <mfr01606587@alibaba-inc.com>
2023-02-14 15:10:13 +0800

    引入 @mzy123/customcomponentlib

--- src/views/Car/index.js
+++ src/views/Car/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Tag} from '@mzy123/customcomponentlib'
 
 function Car (){
   return (
-    <div>Car</div>
+    <div>
+      <Tag color="#06c">标签</Tag>
+      <Tag color="red">标签</Tag>
+      <Tag color="orange">标签</Tag>
+      <Tag>标签</Tag>
+      <Tag color="green" closable>
+        标签
+      </Tag>
+      <Tag closable>标签</Tag>
+    </div>
   )
 }
 
