602f56be464bb30c8a965f3064c11f68de955b9f
孟凡荣 <mfr01606587@alibaba-inc.com>
2023-02-16 20:03:46 +0800

    feat: react-monorepo

--- src/views/Car/index.js
+++ src/views/Car/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import {Tag} from '@mzy123/customcomponentlib'
+import {Tag, Button} from '@mzy123/customcomponentlib'
 
 function Car (){
   return (
     <div>
-      <Tag color="#06c">标签</Tag>
-      <Tag color="red">标签</Tag>
-      <Tag color="orange">标签</Tag>
-      <Tag>标签</Tag>
-      <Tag color="green" closable>
+      <div>
+        <Tag color="#06c">标签</Tag>
+        <Tag color="red">标签</Tag>
+        <Tag color="orange">标签</Tag>
+        <Tag>标签</Tag>
+        <Tag color="green" closable>
         标签
-      </Tag>
-      <Tag closable>标签</Tag>
+        </Tag>
+        <Tag closable>标签</Tag>
+      </div>
+
+
+      <div>
+        <h1>Button</h1>
+        <Button type="primary">点击</Button>
+      </div>
     </div>
   )
 }
