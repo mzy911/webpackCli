2beca0ba1f0fbfaa452654b1294e9538bdff7f87
weiyy <weiyy>
2023-12-12 22:37:22 +0800

    feat: webpack-style

--- config/baseConfig/webpack.style.js
+++ config/baseConfig/webpack.style.js
@@ -13,12 +13,32 @@ if (process.env.NODE_ENV === "production"){
 
 const getStyleLoaders = (pre) => {
   return [
-    // 创建 style 标签插入html中(生成环境使用压缩)
+    // style-loader
+    // 1、将结果以style标签的方式插入DOM树中
+    // 2、作用如下，以 style 的形式插入到 header 中
+    // module.exports = function (content) {
+    //   return `
+    //     const style = document.createElement('style');
+    //     style.innerHTML = '${content}';
+    //     document.head.appendChild(style);
+    //   `;
+    // };
+    // mini-css-extract-plugin
+    // 1、将 CSS 提取到单独的文件中
+    // 2、为每个包含 CSS 的 JS 文件创建一个 CSS 文件
+    // 3、它支持按需加载 CSS 和 SourceMaps
     cssExtract,
-    // 将 Css 文件编译成 Webpack 能识别的模块
+    // css-loader：从 css 文件内解析出样式和资源、编译成 JavaScript 模块、最后导出
+    // 1、从 css 文件中读取 css 代码，并将解析成 JavaScript 可识别的模块
+    // - 处理导入 @import 语句，递归检查导入的 css 文件，并将他们合到主 CSS 文件中
+    // - 处理 url() 和 背景图片，将 background 资源转为模块，在 webpack 中进行加载和处理
+    // - 编译 css 代码
+    // 2、导出处理后的 JavaScript 模块
+    // - 返回处理好、完整的 "JavaScript" 模块
+    // - 交给 style-loader 将结果以 style 标签的方式插入 DOM 树中
     "css-loader",
     {
-      // 解决样式的兼容性问题
+      // 解决浏览器样式的兼容性问题
       loader: "postcss-loader",
       options: {
         postcssOptions: {
